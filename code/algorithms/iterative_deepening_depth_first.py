b342c858878b9ab5c503a14a5eaa98e1dad23723
Quint-Langeveld <quintlangeveld@hotmail.com>
2018-11-29 12:39:09 +0100

    some adds

--- code/algorithms/iterative_deepening_depth_first.py
+++ code/algorithms/iterative_deepening_depth_first.py
@@ -1,1 +1,26 @@
 #  hii deze is voor itterative deepening depth first
+from classes.field import Field
+from classes.cell import Cell
+import copy
+
+
+def itterative_deepening_depth_first(old_field):
+    new_fields = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return new_fields
